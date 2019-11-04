20ccf80ac0f79fd170ffd223d4c5fcc52111c34e
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2019-11-04 17:58:26 -0500

    Validando formularios

--- lib/src/Pages/ProductPage.dart
+++ lib/src/Pages/ProductPage.dart
@@ -1,7 +1,15 @@
+import 'package:crudfirebaseflutter/src/Utils/Utils.dart' as utils;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,11 +30,12 @@ class ProductPage extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(15.0),
           child: Form(
+            key: formKey,
             child: Column(
               children: <Widget>[
-              	_createName(),
-	              _createPrice(),
-	              _createRaisedButton(),
+                _createName(),
+                _createPrice(),
+                _createRaisedButton(),
               ],
             ),
           ),
@@ -39,6 +48,13 @@ class ProductPage extends StatelessWidget {
     final textFormField = TextFormField(
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(labelText: 'Producto'),
+      validator: (value) {
+        if (value.length < 3) {
+          return 'El nombre debe de tener mas de 3 carácteres';
+        } else {
+          return null;
+        }
+      },
     );
 
     return textFormField;
@@ -48,23 +64,28 @@ class ProductPage extends StatelessWidget {
     final textFormField = TextFormField(
       keyboardType: TextInputType.numberWithOptions(decimal: true),
       decoration: InputDecoration(labelText: 'Precio'),
+      validator: (value) {
+        return (utils.isNumeric(value)) ? null : 'Solo números por favor';
+      },
     );
 
     return textFormField;
   }
-  
+
   Widget _createRaisedButton() {
     final raisedButton = RaisedButton.icon(
-	    shape: RoundedRectangleBorder(
-		    borderRadius: BorderRadius.circular(20.0)
-	    ),
-	    color: Colors.deepPurple,
-	    textColor: Colors.white,
-	    label: Text('Guardar'),
-	    icon: Icon(Icons.save),
-	    onPressed: (){},
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      label: Text('Guardar'),
+      icon: Icon(Icons.save),
+      onPressed: _submit,
     );
 
     return raisedButton;
+  }
+
+  void _submit() {
+    if (!formKey.currentState.validate()) return;
   }
 }
---
+++ lib/src/Utils/Utils.dart
@@ -0,0 +1,5 @@
+bool isNumeric(String value) {
+  if (value.isEmpty) return false;
+  final n = num.tryParse(value);
+  return (n == null) ? false : true;
+}
